4f783ffcd54913d8d068703909bbe5cd8e6810a8
tommylau523 <tommylau523@gmail.com>
2015-11-17 22:20:03 -0500

    Initialize blank project

---
+++ DrawingBook/DrawingBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingBook
+//
+//  Created by Tommy Lau on 11/17/15.
+//  Copyright © 2015 Tommy Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DrawingBook/DrawingBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DrawingBook
+//
+//  Created by Tommy Lau on 11/17/15.
+//  Copyright © 2015 Tommy Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

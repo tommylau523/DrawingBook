193bf5e94da05fb6e29a6950354070fc8552abcd
William Willsey <wwillsey414@gmail.com>
2015-12-01 19:23:20 -0500

    trying to calc extrinsics

--- DrawingBook/DrawingBook/ViewController.mm
+++ DrawingBook/DrawingBook/ViewController.mm
@@ -7,29 +7,30 @@
 //
 
 #import "ViewController.h"
-
+#include <stdlib.h>
+#include <iostream>
 #ifdef __cplusplus
 #include <opencv2/opencv.hpp>
-#include "opencv2/highgui/ios.h"
+//#include "opencv2/highgui/ios.h"
 #include <opencv2/features2d/features2d.hpp>
 #include "opencv2/nonfree/nonfree.hpp"
+//#include "armadillo"
 #endif
 
 // Include stdlib.h and std namespace so we can mix C++ code in here
-#include <stdlib.h>
-#include <iostream>
-using namespace std;
 
+using namespace std;
 
 @interface ViewController (){
     UIImageView *liveView_; // Live output from the camera
     CvVideoCamera *videoCamera_; // OpenCV wrapper class to simplfy camera access through AVFoundation;
-    cv::vector<cv::KeyPoint> K_template;
+    cv::vector<cv::KeyPoint> template_keypoints;
     cv::Mat template_im;
     cv::Mat template_descriptor;
     cv::Ptr<cv::FeatureDetector> detector;
     cv::Ptr<cv::DescriptorExtractor> extractor;
     cv::Ptr<cv::BFMatcher> matcher;
+    cv::Mat intrinsics;
 }
 @property (nonatomic, retain) CvVideoCamera* videoCamera;
 @end
@@ -44,9 +45,14 @@ using namespace std;
     extractor = new cv::OrbDescriptorExtractor;
     matcher = new cv::BFMatcher(cv::NORM_HAMMING, true);
     template_im = [self cvMatFromUIImage:template_image];
-    K_template = getKeyPoints(template_im, detector);
-    extractor->compute(template_im, K_template, template_descriptor);
-    
+    template_keypoints = getKeyPoints(template_im, detector);
+    extractor->compute(template_im, template_keypoints, template_descriptor);
+    intrinsics = cv::Mat::zeros(3,3,CV_64F);
+    intrinsics.at<double>(0,0) = 3043.72;
+    intrinsics.at<double>(1,1) = 3043.72;
+    intrinsics.at<double>(2,2) = 1;
+    intrinsics.at<double>(0,2) = 1196;
+    intrinsics.at<double>(1,2) = 1604;
     
     // 1. Setup the your OpenCV view, so it takes up the entire App screen......
     int view_width = self.view.frame.size.width;
@@ -85,76 +91,119 @@ using namespace std;
 
 - (void)processImage:(cv::Mat&) image;
 {
+    
     // Do some OpenCV stuff with the image
     cv::Mat image_copy, gray, template_gray;
-    cv::Mat descriptor_output;
+    cv::Mat image_descriptor;
     std::vector<cv::DMatch> matches;
     
     cvtColor(image, image_copy, CV_BGRA2BGR);
     cvtColor(image_copy, gray, CV_BGRA2GRAY);
     cvtColor(template_im, template_gray, CV_BGRA2GRAY);
     
-    
-    cv::vector<cv::KeyPoint> K = getKeyPoints(image_copy, detector);
-    extractor->compute(image_copy, K, descriptor_output);
-    
-    matcher->match(descriptor_output, template_descriptor, matches);
-    
-    
-    
-    
-    
-    
-    double max_dist = 0; double min_dist = 300;
-    
-    //-- Quick calculation of max and min distances between keypoints
-    for( int i = 0; i < matches.size(); i++ )
-    { double dist = matches[i].distance;
-        if( dist < min_dist ) min_dist = dist;
-        if( dist > max_dist ) max_dist = dist;
-    }
-    
-    printf("-- Max dist : %f \n", max_dist );
-    printf("-- Min dist : %f \n", min_dist );
-    
-    std::vector< cv::DMatch > good_matches;
-    
-    for( int i = 0; i < matches.size(); i++ )
-    { if( matches[i].distance < 4*min_dist )
-    { good_matches.push_back( matches[i]); }
-    }
-    
-    
-    cv::vector<cv::Point2f> source;
-    cv::vector<cv::Point2f> dest;
-    
-    for(int i = 0; i < good_matches.size(); i++){
-        source.push_back(K[good_matches[i].queryIdx].pt);
-        dest.push_back(K_template[good_matches[i].trainIdx].pt);
-    }
-    if(good_matches.size() >= 10){
-        cv::Mat H = cv::findHomography(source, dest, CV_RANSAC);
-        //cout << H << endl;
-    
-    
-    std::vector<cv::Point2f> obj_corners(4);
-    obj_corners[0] = cvPoint(0,0);
-    obj_corners[1] = cvPoint( template_im.cols, 0 );
-    obj_corners[2] = cvPoint( template_im.cols, template_im.rows );
-    obj_corners[3] = cvPoint( 0, template_im.rows );
-    std::vector<cv::Point2f> scene_corners(4);
-    cv::perspectiveTransform( obj_corners, scene_corners, H);
-        cout << H << endl;
-        
-    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
-    cv::line( image_copy, scene_corners[0], scene_corners[1], cv::Scalar(0, 255, 0), 4 );
-    cv::line( image_copy, scene_corners[1], scene_corners[2], cv::Scalar( 0, 255, 0), 4 );
-    cv::line( image_copy, scene_corners[2], scene_corners[3], cv::Scalar( 0, 255, 0), 4 );
-    cv::line( image_copy, scene_corners[3], scene_corners[0], cv::Scalar( 0, 255, 0), 4 );
+    @try{
+        cv::vector<cv::KeyPoint> image_keypoints = getKeyPoints(image_copy, detector);
+        extractor->compute(image_copy, image_keypoints, image_descriptor);
+        if(image_descriptor.cols < 20){
+            return;
+        }
+        matcher->match(template_descriptor, image_descriptor, matches);
 
+        
+        double max_dist = 0; double min_dist = 300;
+        
+        //-- Quick calculation of max and min distances between keypoints
+        for( int i = 0; i < matches.size(); i++ )
+        { double dist = matches[i].distance;
+            if( dist < min_dist ) min_dist = dist;
+            if( dist > max_dist ) max_dist = dist;
+        }
+        
+        std::vector< cv::DMatch > good_matches;
+        
+        for( int i = 0; i < matches.size(); i++ )
+        { if( matches[i].distance < 3*min_dist )
+        { good_matches.push_back( matches[i]); }
+        }
+        
+        
+        cv::vector<cv::Point2f> source;
+        cv::vector<cv::Point2f> dest;
+        
+        for(int i = 0; i < good_matches.size(); i++){
+            source.push_back(template_keypoints[good_matches[i].queryIdx].pt);
+            dest.push_back(image_keypoints[good_matches[i].trainIdx].pt);
+        }
+        if(good_matches.size() >= 10){
+            cv::Mat H = cv::findHomography(source, dest, CV_RANSAC);
+            cv::Mat E = get_extrinsics(intrinsics, H);
+            cv::Mat camera = intrinsics*E;
+            
+            std::vector<cv::Point3f> proj_corners(4);
+            std::vector<cv::Point2f> scene_proj_corners(4);
+            proj_corners[0] = cv::Point3f(0,0,0);
+            proj_corners[1] = cv::Point3f( template_im.cols, 0, 0 );
+            proj_corners[2] = cv::Point3f( template_im.cols, template_im.rows, 0 );
+            proj_corners[3] = cv::Point3f( 0, template_im.rows, 0 );
+            cv::perspectiveTransform(proj_corners, scene_proj_corners, camera);
+            
+            cv::line( image_copy, scene_proj_corners[0], scene_proj_corners[1], cv::Scalar(255, 255, 0), 4 );
+            cv::line( image_copy, scene_proj_corners[1], scene_proj_corners[2], cv::Scalar( 255, 255, 0), 4 );
+            cv::line( image_copy, scene_proj_corners[2], scene_proj_corners[3], cv::Scalar( 255, 255, 0), 4 );
+            cv::line( image_copy, scene_proj_corners[3], scene_proj_corners[0], cv::Scalar( 255, 255, 0), 4 );
+            
+            
+            std::vector<cv::Point2f> obj_corners(4);
+            obj_corners[0] = cvPoint(0,0);
+            obj_corners[1] = cvPoint( template_im.cols, 0 );
+            obj_corners[2] = cvPoint( template_im.cols, template_im.rows );
+            obj_corners[3] = cvPoint( 0, template_im.rows );
+            std::vector<cv::Point2f> scene_corners(4);
+            cv::perspectiveTransform( obj_corners, scene_corners, H);
+            //cout << H << endl;
+            //cout << scene_corners << endl;
+            //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+            //cv::line( image_copy, scene_corners[0], scene_corners[1], cv::Scalar(0, 255, 0), 4 );
+            //cv::line( image_copy, scene_corners[1], scene_corners[2], cv::Scalar( 0, 255, 0), 4 );
+            //cv::line( image_copy, scene_corners[2], scene_corners[3], cv::Scalar( 0, 255, 0), 4 );
+            //cv::line( image_copy, scene_corners[3], scene_corners[0], cv::Scalar( 0, 255, 0), 4 );
+            
+        }
+    } @catch(NSException *theException) {
+        cout << theException.name << endl;
+        cout << theException.reason << endl;
     }
+    
     //cv::drawMatches(gray, K, template_gray, K_template, good_matches, image);
     cvtColor(image_copy, image, CV_BGR2BGRA);
+    
+}
+
+cv::Mat get_extrinsics(cv::Mat intrinsics, cv::Mat H){
+    cv::Mat U, l, V, Hp, t, Omega, o;
+    cout << intrinsics << endl;
+    Hp = intrinsics.inv() * H;
+    t = Hp.col(2);
+    Hp = Hp.colRange(0, 2);
+    
+    cv::SVD::compute(Hp, l, U, V, cv::SVD::FULL_UV);
+    
+    cv::Mat L = cv::Mat::eye(3, 2, CV_64F);
+    Omega = U*L*V.t();
+    o = Omega.col(0).cross(Omega.col(1));
+    cv::hconcat(Omega, o, Omega);
+    
+    Omega.col(2) *= cv::determinant(Omega.t()*Omega);
+    float scale = 0;
+    for(int m = 0; m < 3; m++){
+        for(int n = 0; n < 2; n++){
+            scale += Hp.at<double>(n,m) / Omega.at<double>(n,m);
+        }
+    }
+    scale /= 6.0;
+    t /= scale;
+    cv::hconcat(Omega, t, Omega);
+    return Omega;
 }
 
 
@@ -171,7 +220,7 @@ cv::vector<cv::KeyPoint> getKeyPoints(cv::Mat &img, cv::Ptr<cv::FeatureDetector>
 
 - (NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
 {
-    return UIInterfaceOrientationMaskLandscape;
+    return UIInterfaceOrientationMaskPortrait;
 }
 
 // Member functions for converting from cvMat to UIImage
